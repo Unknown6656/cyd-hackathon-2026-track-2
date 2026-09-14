f3008ba2fdba352e012b18ad37522c776156a170
jaleut <jaleut@nudel.io>
2026-09-14 16:42:00 +0200

    update embed

--- app/src/app/embed.py
+++ app/src/app/embed.py
@@ -4,17 +4,15 @@ class EmbeddingModel:
 
     def __init__(self, base_url: str, model_name: str, api_key: str | None):
         self.model_name = model_name
-        self.client = AsyncClient(
-            host=base_url,
-            headers={"Authorization": f"Bearer {api_key}"},
-        )
+        self.base_url = base_url
+        self.api_key = api_key
 
     async def embed(self, text: str):
         async with AsyncClient() as aclient:
             response = await aclient.post(
-                "http://localhost:11434/v1/embeddings",
+                self.base_url,
                 json={"model": self.model_name, "input": text},
-                headers={"Authorization": "Bearer ollama"}
+                headers={"Authorization": f"Bearer {self.api_key}"}
             )
             response.raise_for_status()
             data = response.json()
